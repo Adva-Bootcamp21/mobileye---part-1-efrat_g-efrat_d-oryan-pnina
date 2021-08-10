fd17df4cfa1324a3c014e29be2d9a6da7fa1c64b
OryanTamsut <tamsut2777@gmail.com>
2021-08-10 11:16:58 +0300

    color and max

--- part1_api.py
+++ part1_api.py
@@ -19,7 +19,11 @@ except ImportError:
 
 
 def get_kernel_circle(kernel_number):
-    circle_img = Image.open(f'kernel{kernel_number}.png')
+    if kernel_number == '10':
+        circle_img = Image.open(f'kernel9.png')
+        circle_img = circle_img.transpose(Image.ROTATE_180)
+    else:
+        circle_img = Image.open(f'kernel{kernel_number}.png')
     kernel_circle = np.asarray(circle_img)[:, :, 0]
     kernel_circle = kernel_circle.astype(float32)
     kernel_circle -= 100
@@ -33,10 +37,9 @@ def get_kernel_circle(kernel_number):
 
 def get_dots(image, threshold):
     c_image_converted = image.astype(np.float32)[10:-5, 10:-10]
-    lights_indices = np.argwhere(threshold < c_image_converted)
-    y_array = lights_indices[:, 0] + 10
-    x_array = lights_indices[:, 1] + 10
-    return x_array, y_array
+    filtered = maximum_filter(c_image_converted, 30)
+    lights_indices = np.argwhere((threshold < c_image_converted) & (filtered == c_image_converted))
+    return lights_indices + 10
 
 
 def find_tfl_by_layer(c_image, layer, t_small, t_big):
@@ -47,33 +50,39 @@ def find_tfl_by_layer(c_image, layer, t_small, t_big):
     # find_small_tfl
     kernel_circle = get_kernel_circle("6")
     res = sg.convolve(c_image_bw_array, kernel_circle, mode='same', method='auto')
-    threshold = np.max(res) - t_small
+    threshold = np.max(res[10:-5, 10:-10]) - t_small
     dots_small = get_dots(res, threshold)
 
     # find_big_tfl
     kernel_circle = get_kernel_circle("4")
     res = sg.convolve(c_image_bw_array, kernel_circle, mode='same', method='auto')
-    threshold = np.max(res) - t_big
+    threshold = np.max(res[10:-5, 10:-10]) - t_big
     dots_big = get_dots(res, threshold)
-    dots = np.hstack([dots_small, dots_big])
+    dots = np.vstack([dots_small, dots_big])
     return dots
 
 
-def find_tfl_lights2(c_image: np.ndarray, **kwargs):
+def find_tfl_lights(c_image: np.ndarray, **kwargs):
     """
     Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement
     :param c_image: The image itself as np.uint8, shape of (H, W, 3)
     :param kwargs: Whatever config you want to pass in here
     :return: 4-tuple of x_red, y_red, x_green, y_green
     """
-    ### WRITE YOUR CODE HERE ###
-    ### USE HELPER FUNCTIONS ###
-
+    dots_red_layer = find_tfl_by_layer(c_image, 0, 1000, 5000)
     dots_green_layer = find_tfl_by_layer(c_image, 1, 1000, 5000)
-    dots_red_layer = find_tfl_by_layer(c_image, 0, 1000, 6400)
-
-    dots = np.hstack([dots_green_layer, dots_red_layer])
-    return dots[0], dots[1], [], []
+    filtered_red_dots, filtered_green_dots = np.array([[0, 0]]), np.array([[0, 0]])
+    for dot in dots_red_layer:
+        if c_image[dot[0], dot[1]][0] >= c_image[dot[0], dot[1]][1]:
+            filtered_red_dots = np.append(filtered_red_dots, [[dot[0], dot[1]]], axis=0)
+        else:
+            filtered_green_dots = np.append(filtered_green_dots, [[dot[0], dot[1]]], axis=0)
+    for dot in dots_green_layer:
+        if c_image[dot[0], dot[1]][1] > c_image[dot[0], dot[1]][0]:
+            filtered_green_dots = np.append(filtered_green_dots, [[dot[0], dot[1]]], axis=0)
+        else:
+            filtered_red_dots = np.append(filtered_red_dots, [[dot[0], dot[1]]], axis=0)
+    return filtered_red_dots[1:, 1], filtered_red_dots[1:, 0], filtered_green_dots[1:, 1], filtered_green_dots[1:, 0]
 
 
 ### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
@@ -104,9 +113,10 @@ def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
 
     show_image_and_gt(image, objects, fig_num)
 
-    red_x, red_y, green_x, green_y = find_tfl_lights2(image, some_threshold=42)
+    red_x, red_y, green_x, green_y = find_tfl_lights(image, some_threshold=42)
     plt.plot(red_x, red_y, 'ro', markersize=4)
     plt.plot(green_x, green_y, 'go', markersize=4)
+
 
 def main(argv=None):
     """It's nice to have a standalone tester for the algorithm.
